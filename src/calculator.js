4cbee189b510e504be0f10520660632ff0831276
Grace Kostanich <fl5812597@gmail.com>
2022-06-24 16:43:55 -0700

    Add passing GREEN test | correctly return Mercury age and corresponding life expectancy message

--- src/calculator.js
+++ src/calculator.js
@@ -2,9 +2,14 @@ export default class Calc {
   constructor(age, lifeExpect) {
     this.age = age;
     this.lifeExpect = lifeExpect;
+  }; 
+  CalcMercury() {
+    let mercuryAge = 0
+    let ageAndExpect = ""
+    if (this.age < this.lifeExpect) {
+      mercuryAge = this.age * .24 
+      ageAndExpect = mercuryAge + "; You're under your life expectancy on Mercury!"
+    };
+    return ageAndExpect
   };
-
-//   // Calculate() {
-//   //   //function body
-//   // };
 };
