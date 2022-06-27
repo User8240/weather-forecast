7a14ccd79d6b7cb7266bd0aa6cf94b5deff81038
Grace Kostanich <fl5812597@gmail.com>
2022-06-26 17:19:52 -0700

    Add passing GREEN test | correctly return Venus age and corresponding life expectancy message

--- src/calculator.js
+++ src/calculator.js
@@ -9,11 +9,25 @@ export default class Calc {
   
     mercuryAge = this.age / .24 
     if (this.lifeExpect > mercuryAge) {
-      ageAndExpect = mercuryAge + "; You're under your life expectancy on Mercury!"
+      ageAndExpect = mercuryAge.toFixed(0) + "; You're under your life expectancy on Mercury!"
     } else if (this.lifeExpect < mercuryAge) {
-      ageAndExpect = mercuryAge + "; You're over your life expectancy on Mercury!"
+      ageAndExpect = mercuryAge.toFixed(0) + "; You're over your life expectancy on Mercury!"
     };
     return ageAndExpect
   };
+
+  CalcVenus() {
+    let venusAge = 0
+    let ageAndExpect = ""
+  
+    venusAge = this.age / .62 
+    if (this.lifeExpect > venusAge) {
+      ageAndExpect = venusAge.toFixed(0) + "; You're under your life expectancy on Venus!"
+    } else if (this.lifeExpect < venusAge) {
+      ageAndExpect = venusAge.toFixed(0) + "; You're over your life expectancy on Venus!"
+    };
+    return ageAndExpect
+  };
+
 };
   
