11a5a13abc3a9e057eecb46599c20cae2cf527cc
Grace Kostanich <fl5812597@gmail.com>
2022-06-26 20:35:54 -0700

    Add RED test | correctly return Venus age, years left, and corresponding life expectancy message

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -10,9 +10,9 @@ describe('Calc', () => {
     const calcObject = new Calc(30,100);
     expect(calcObject.CalcMercury()).toEqual("You are 125 years old on Mercury. You have 292 years left to live!");
   });
-  test('should correctly return Venus age and corresponding life expectancy message', () => {
+  test('should correctly return Venus age, years left, and corresponding life expectancy message', () => {
     const calcObject = new Calc(30,100);
-    expect(calcObject.CalcVenus()).toEqual("48; You're under your life expectancy on Venus!");
+    expect(calcObject.CalcVenus()).toEqual("You are 48 years old on Venus. You have 113 years left to live!");
   });
   test('should correctly return Mars age and corresponding life expectancy message', () => {
     const calcObject = new Calc(30,100);
